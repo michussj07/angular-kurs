32416e29f990616b42056b5cc7afca88352b157a
Michal <michussj07@wp.pl>
2019-10-14 19:05:04 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-kurs';
+  title = 'Angulara';
+
+  days = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota',
+  'Niedziela'];
+
+  dogs = new Array<Dog>();
+
+  constructor(){
+    this.dogs.push(new Dog('Reksio', 4), new Dog('Łatek', 3),
+    new Dog('Maksiu', 5));
+  }
+
+
+}
+
+
+class Dog {
+  constructor(public name: string, public age: number) {
+  }
 }
